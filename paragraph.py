ef15436e93eddfd227ab4eeb858aa42c4bc6c691
VISHANT PANDIT <vishantpandit80@gmail.com>
2026-03-05 20:59:50 +0530

    Add files via upload

---
+++ paragraph.py
@@ -0,0 +1,61 @@
+# a=('''thats a myth
+#       python learner
+#       in imr college''')
+# print("what you want search in this paragraph:-")
+# i=input()
+# if i  in a:
+#     print("exists")
+    
+# else:
+#     print(" not exists")
+    
+                       #    capitalize string
+# a='''python learner'''
+# u=a.isupper()
+# print(u)
+
+
+# a='''python learner'''
+# u=a.upper()
+# print(u)
+
+
+# a='''python learner'''
+# u=a.islower()
+# print(u)
+
+# print("enter the string")
+# a=input()
+# u=a.isupper()
+# def check():
+#     if u in 
+
+
+                   #    for upper and lower
+def for_uppercase():
+    print("enter your string:-")
+    a=input()
+    
+    if a.isupper():
+        print("already in uppercase")
+    else:
+        u=a.upper()
+        print( "change in upper case:-",u)
+
+                                     # for_uppercase()
+
+                                     # lower
+
+
+def for_lowercase():
+    print("enter your string:-")
+    a=input()
+    
+    if a.islower():
+        print("already in lowercase")
+    else:
+        u=a.lower()
+        print("change in lower case:-",u)
+
+
+                                    # for_lowercase()
