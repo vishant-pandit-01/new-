1c3dae73e70294b510c4f76655d2df68769df4cd
VISHANT PANDIT <vishantpandit80@gmail.com>
2026-03-07 21:45:19 +0530

    Update Random.py

--- Random.py
+++ Random.py
@@ -3,18 +3,12 @@ import random
 # ----------fot random digit----------
 print(random.randrange(1,1000))
 print(x)
-     output:- 678
 
 ----------for random no. in given data---------
 print(random.choice(['A','B','C','D','E']))  
-       utput:- B
 
 # ----------for multiple choice----------
 print(random.choice(['red','green','yellow']))
-output:- green
 
 # -----------for multiple sample----------
 print(random.sample(['1','2','3','4','5','6'],3))
-      output:- ['5','2','4']
-
-
